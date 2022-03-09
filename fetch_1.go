c51b75e22a1d867130bff3b0a5c583b9d43da0e0
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2022-03-09 20:44:39 +1100

    fetch url and web server

---
+++ fetch_1.go
@@ -0,0 +1,42 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			fmt.Println("url does not have prefix")
+			url = "http://" + url
+		} else {
+			fmt.Println("url already is prefixed")
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		httpStatusCode := resp.StatusCode
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("The http request code came back with status code : %v \n", httpStatusCode)
+	}
+}
+
+//!-
